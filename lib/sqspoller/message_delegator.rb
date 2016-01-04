a13721340d0929d862ccf25d735d4975f7e1ffbf
anshul-fk <anshul@fourkites.com>
2016-01-04 15:34:36 +0530

    Fixed bug in sqspoller

--- lib/sqspoller/message_delegator.rb
+++ lib/sqspoller/message_delegator.rb
@@ -22,28 +22,27 @@ module Sqspoller
     def process(queue_controller, message, queue_name)
       @semaphore.synchronize {
         @pending_schedule_tasks +=1
-        if @connection_pool.queue_length + @pending_schedule_tasks >= @max_allowed_queue_size
+        if @pending_schedule_tasks >= @max_allowed_queue_size
           @logger.info "Entered wait state, connection_pool size reached max threshold, pending_schedule_tasks=#{@pending_schedule_tasks}"
-          while @connection_pool.queue_length > @worker_thread_pool_size || @connection_pool.queue_length + @pending_schedule_tasks >= @max_allowed_queue_size
+          while @connection_pool.queue_length > @worker_thread_pool_size || @pending_schedule_tasks >= @max_allowed_queue_size
             sleep(0.01)
           end
           @logger.info "Exiting wait state, connection_pool size reached below worker_thread_pool_size, pending_schedule_tasks=#{@pending_schedule_tasks}"
         end
       }
-      begin
-        @logger.info "Scheduling worker task for message: #{message.message_id}"
+      @logger.info "Scheduling worker task for message: #{message.message_id}"
 
+      begin
         @connection_pool.post do
           begin
             @logger.info "Starting worker task for message: #{message.message_id}"
             @worker_task.process(message.body, message.message_id)
-            @pending_schedule_tasks -= 1
             @logger.info "Finished worker task for message: #{message.message_id}"
             queue_controller.delete_message message.receipt_handle
           rescue Exception => e
-            @pending_schedule_tasks -= 1
             @logger.info "Caught error for message: #{message}, error: #{e.message}, #{e.backtrace.join("\n")}"
           end
+          @pending_schedule_tasks -= 1
         end
       rescue Concurrent::RejectedExecutionError => e
         @pending_schedule_tasks -= 1
